090e875650f7f0463bb732d28cda3d1ab5489a4e
Frederick Kramer Dal Pra <24514819+FrederickKDP@users.noreply.github.com>
2021-12-18 20:16:42 -0800

    Update label

--- JsonReader.gd
+++ JsonReader.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+signal file_read(quote)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +13,8 @@ func _ready():
 	file.open("res://Quotes.txt", File.READ)
 	var content = file.get_as_text()
 	var json_read : JSONParseResult = JSON.parse(content)
-	print(json_read.result["entities"][0]["name"])
+	var txt = (json_read.result["entities"][0]["name"])
+	emit_signal("file_read", txt)
 	file.close()
 	#return content
 
---
+++ UpdateLabel.gd
@@ -0,0 +1,5 @@
+extends Label
+
+
+func _on_Node_file_read(quote):
+	text = quote
