54a0a408be1898ae8d10cb182c4384e284a7d661
Frederick Kramer Dal Pra <24514819+FrederickKDP@users.noreply.github.com>
2021-12-22 11:34:22 -0800

    Advance quotes over time

--- JsonReader.gd
+++ JsonReader.gd
@@ -1,28 +1,37 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 signal file_read(quote)
+
+var quotes_col : JSONParseResult
+var current_quote : int = 0
+var max_quotes : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().get_root().set_transparent_background(true)
 	randomize()
 	var file = File.new()
 	file.open("res://Quotes.txt", File.READ)
 	var content = file.get_as_text()
-	var json_read : JSONParseResult = JSON.parse(content)
-	var entities_size : int = json_read.result.size()
-	var entity = json_read.result[randi()%entities_size]
-	var txt = (entity["author"]+" "+entity["quote"])
-	print(entity["tags"])
-	emit_signal("file_read", txt)
+	quotes_col  = JSON.parse(content)
+	max_quotes = quotes_col.result.size()
+	current_quote = randi()%max_quotes
+	var entity = quotes_col.result[current_quote]
+	show_quote(entity["quote"], entity["author"])
 	file.close()
 	#return content
 
+func next_quote():
+	current_quote+=1
+	if(current_quote>=max_quotes):
+		current_quote=0
+	var entity = quotes_col.result[current_quote]
+	show_quote(entity["quote"], entity["author"])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func show_quote(quote : String, author:String):
+	var txt = author+" "+quote
+	emit_signal("file_read", txt)
+
+
+func _on_Timer_timeout():
+	next_quote()
---
+++ MakeBorderless.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_Button_button_down():
+	OS.window_borderless = !OS.window_borderless
