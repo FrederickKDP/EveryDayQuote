4a662e90a1bed7d99696dbb885bd07e8d89f7d8b
Frederick Kramer Dal Pra <24514819+FrederickKDP@users.noreply.github.com>
2021-12-23 23:48:51 -0800

    Author & tags added

--- JsonReader.gd
+++ JsonReader.gd
@@ -1,10 +1,14 @@
 extends Node
 
-signal file_read(quote)
+#signal file_read(quote)
 
 var quotes_col : JSONParseResult
 var current_quote : int = 0
 var max_quotes : int = 0
+
+onready var quote_label : Label = $Control/Quote
+onready var author_label : Label = $Control/Author
+onready var tags_label : Label = $Control/Tags
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,20 +21,22 @@ func _ready():
 	max_quotes = quotes_col.result.size()
 	current_quote = randi()%max_quotes
 	var entity = quotes_col.result[current_quote]
-	show_quote(entity["quote"], entity["author"])
+	show_quote(entity)
 	file.close()
 	#return content
 
 func next_quote():
+	print("call")
 	current_quote+=1
 	if(current_quote>=max_quotes):
 		current_quote=0
 	var entity = quotes_col.result[current_quote]
-	show_quote(entity["quote"], entity["author"])
+	show_quote(entity)
 
-func show_quote(quote : String, author:String):
-	var txt = author+" "+quote
-	emit_signal("file_read", txt)
+func show_quote(entity : Dictionary):
+	quote_label.text = entity["quote"]
+	author_label.text = entity["author"]
+	tags_label.text = entity["tags"][0]
 
 
 func _on_Timer_timeout():
