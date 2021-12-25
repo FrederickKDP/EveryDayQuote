40dfdd65c76c0dd7b4aa43f5ebb3d2f40fd70227
Frederick Kramer Dal Pra <24514819+FrederickKDP@users.noreply.github.com>
2021-12-24 21:15:37 -0800

    Screen resolution supported and tag commas

--- JsonReader.gd
+++ JsonReader.gd
@@ -2,14 +2,16 @@ extends Node
 
 #signal file_read(quote)
 
+export(float) var fade_time = 1
+
 var quotes_col : JSONParseResult
 var current_quote : int = 0
 var max_quotes : int = 0
 
 onready var group_label : Control = $Control
 onready var quote_label : Label = $Control/Quote
-onready var author_label : Label = $Control/Author
-onready var tags_label : Label = $Control/Tags
+onready var author_label : Label = $Control/Quote/Author
+onready var tags_label : Label = $Control/Quote/Tags
 
 onready var fadeIn : Tween = $FadeIn
 onready var fadeOut : Tween = $FadeOut
@@ -19,8 +21,8 @@ onready var timer : Timer = $NextQuote
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Fades
-	var _inter = fadeIn.interpolate_property(group_label, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	_inter = fadeOut.interpolate_property(group_label, "modulate", Color(1,1,1,0), Color(1,1,1,1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	var _inter = fadeIn.interpolate_property(group_label, "modulate", Color(1,1,1,1), Color(1,1,1,0), fade_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	_inter = fadeOut.interpolate_property(group_label, "modulate", Color(1,1,1,0), Color(1,1,1,1), fade_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	
 	get_tree().get_root().set_transparent_background(true)
 	randomize()
@@ -32,18 +34,17 @@ func _ready():
 	current_quote = randi()%max_quotes
 	#var entity = quotes_col.result[current_quote]
 	update_quote()
-	fadeOut.start()
+	var _fade = fadeOut.start()
 	file.close()
 	#return content
 
 func next_quote():
-	print("next quote")
 	current_quote+=1
 	if(current_quote>=max_quotes):
 		current_quote=0
 	#fadeIn.reset_all()
-	var _inter = fadeIn.interpolate_property(group_label, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	fadeIn.start()
+	var _inter = fadeIn.interpolate_property(group_label, "modulate", Color(1,1,1,1), Color(1,1,1,0), fade_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	var _fade = fadeIn.start()
 
 func update_quote():
 	var entity = quotes_col.result[current_quote]
@@ -52,8 +53,11 @@ func update_quote():
 	var tags_found = entity["tags"]
 	tags_label.text = "tags:"
 	#TODO: Add commas
-	for i in tags_found:
-		tags_label.text += i+" "
+	var size_loop = tags_found.size()
+	for i in range(size_loop):
+		tags_label.text += tags_found[i]
+		if(i<size_loop-1):
+			tags_label.text += ", "
 
 
 func _on_Timer_timeout():
@@ -67,5 +71,5 @@ func _on_FadeOut_tween_completed(object, key):
 
 func _on_FadeIn_tween_completed(object, key):
 	update_quote()
-	var _inter = fadeOut.interpolate_property(group_label, "modulate", Color(1,1,1,0), Color(1,1,1,1), 1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	var _inter = fadeOut.interpolate_property(group_label, "modulate", Color(1,1,1,0), Color(1,1,1,1), fade_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	fadeOut.start()
