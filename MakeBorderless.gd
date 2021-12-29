8efabea65b6ce14cc165bb720831f10e1220f88a
Frederick Kramer Dal Pra <24514819+FrederickKDP@users.noreply.github.com>
2021-12-28 20:25:33 -0800

    Delayed borderless

--- MakeBorderless.gd
+++ MakeBorderless.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var delay = $DelayedBorder
+
 func _ready():
 	#Make BG transparent
 	get_tree().get_root().set_transparent_background(true)
@@ -10,4 +12,9 @@ func _on_Panel_mouse_entered():
 
 
 func _on_Panel_mouse_exited():
+	delay.stop()
+	delay.start()
+
+
+func _on_DelayedBorder_timeout():
 	OS.window_borderless = true
